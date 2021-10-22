152e2d95ec1ce8a750028f9dd2d099efd0103e85
VladTsimoshchanka <78988772+VladTsimoshchanka@users.noreply.github.com>
2021-10-22 12:32:19 -0400

    Added hours of operation and map

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -3,16 +3,27 @@ var BoarHoursButton = document.getElementById("BoarHoursButton");
 var HamiltonHours = document.getElementById("HamiltonHours");
 var BoarHours = document.getElementById("BoarHours");
 
+BoarHours.style.visibility = "visible";
+//console.log("here");
+
 function changeHours(what){
-    if(what.value == "Ham"){
-        HamiltonHours.style.visibility = "visible";
-        BoarHours.style.visibility = "hidden";
+    if(what == "Ham"){
+        //HamiltonHours.style.visibility = "visible";
+        //BoarHours.style.visibility = "hidden";
+        //HamiltonHours.style.transform = "scale(1.0)";
+        //BoarHours.style.transform = "scale(0.0)";
+        HamiltonHours.style.display = "initial";
+        BoarHours.style.display = "none";
 
     }
     else{
-        BoarHours.style.visibility = "visible";
-        HamiltonHours.style.visibility = "hidden";
-        console.log("herer");
+       // BoarHours.style.visibility = "visible";
+        //HamiltonHours.style.visibility = "hidden";
+        //BoarHours.style.transform = "scale(1.0)";
+        //HamiltonHours.style.transform = "scale(0.0)";
+        HamiltonHours.style.display = "none";
+        BoarHours.style.display = "initial";
+        
     }
 }
 
